29ae83f3ba3f814f208c7814daa9979a46d0b3cd
mohibul <rana.csesust@gmail.com>
2025-07-14 21:53:56 +0600

    first commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+
+import React from 'react';
+   import ReactDOM from 'react-dom/client';
+
+   function App() {
+       return <h1>Hello, React in Laravel with Vite!</h1>;
+   }
+
+   ReactDOM.createRoot(document.getElementById('app')).render(<App />);
